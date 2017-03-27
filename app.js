db3e99100d994a5c903ce0d5ea6e9a74b4229f08
ghigbie <gehigbie@gmail.com>
2017-03-27 22:11:46 +0000

    add more eleborate post route on register

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 const app = express();    
 app.set("view engine", "ejs");
 
+app.user(bodyParser.urlencoded({extended: true}))
 app.use(require("express-session")({
     secret: "Rusty is the best dog ever",
     resave: false,
@@ -46,7 +47,19 @@ app.get("/register", function(req, res){
 });
 //handling user sign up
 app.post("/register", function(req, res){
-    res.send("REGISTER POST ROUTE");
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log("SOMETHING WENT WRONG WITH USER REGISTRATION (POST /register");
+            console.log(err);
+            res.render("register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
 });
 
 
