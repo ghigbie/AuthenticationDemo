d20bf7f7f3543443226e232ad2fcdccba6c9b0e6
ghigbie <gehigbie@gmail.com>
2017-03-27 06:56:21 +0000

    set app.listen to start server

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server is running : )");
+});
